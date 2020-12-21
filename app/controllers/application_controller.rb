341151e7a9f6a554cee73bb00fa7d92100b6c7cb
EliasAHH <eliasiscoding@gmail.com>
2020-12-21 09:15:47 -0800

    Sintra forms complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,9 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    # allows us to use patch / delete request 
+    set :method_override, true 
+    
   end
 
 
@@ -20,9 +23,37 @@ class ApplicationController < Sinatra::Base
     erb :index 
   end 
 
+  #create 
+  post '/books' do
+    @book = Book.create(author:params[:author], title:params[:title], snippet:params[:snippet])
+    redirect to ("/books/#{@book.id}")
+  end 
+
+  #edit
+  get '/books/:id/edit' do
+    @book = Book.find(params[:id])
+    erb :edit 
+  end 
+
+  #patch 
+
+  patch '/books/:id' do
+    binding.pry
+  end 
+
+   #new
+   get '/books/new' do
+    erb :new 
+  end 
+
+  # show 
   get "/books/:id" do 
     @book = Book.find(params[:id])
     erb :show
   end 
+
+  
+ 
+
 
 end
